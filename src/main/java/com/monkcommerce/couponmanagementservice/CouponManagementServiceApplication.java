21b20d2562b29644f78fde865c316834081c153f
dev-workstation <sudeephegde01@gmail.com>
2025-11-15 16:51:08 +0530

    Initial framwork setup commit.

---
+++ src/main/java/com/monkcommerce/couponmanagementservice/CouponManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monkcommerce.couponmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponManagementServiceApplication.class, args);
+    }
+
+}
